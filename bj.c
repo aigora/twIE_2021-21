c50b4e96dd71ed7febcc26791e43f93ec0c8dddc
arturomm <61791305+arturomm@users.noreply.github.com>
2021-05-13 13:10:03 +0200

    main arturo
    creacion del main y declaracion de funciones

---
+++ bj.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _jugador Jugador;
+
+Jugador* inicializarJugador();
+
+void resetearJugador(Jugador* j);
+
+int puntuacionJugador(Jugador* j);
+
+void pintarCartas(Jugador* crupier, Jugador* jugador);
+
+int cogerCarta(Jugador* j);
+
+void cogerCartasCrupier(Jugador* crupier);
+
+void comprobarGanador(Jugador* crupier, Jugador* jugador);
+
+
+int main()
+{
+		//inicializar variables
+		int partida = 1;
+		char opcion;
+
+		Jugador *crupier = inicializarJugador(); 
+		Jugador *jugador = inicializarJugador(); 
+
+        printf("BLACK JACK\n");
+        
+        while(partida){//llegar a la mesa
+        	resetearJugador(crupier);
+        	resetearJugador(jugador);
+
+        	cogerCarta(crupier);
+        	cogerCarta(jugador);
+        	cogerCarta(jugador);
+
+        	while(1){//empezar una partida
+        		pintarCartas(crupier, jugador);
+        		printf("¿Que quieres hacer?\n");
+        		printf("Opciones: Pedir carta (c), Plantarte (p), Doblar (d)\n");
+        		scanf(" %c", &opcion);
+
+        		if (opcion == 'c'){
+		        	printf("Te sale la carta %d\n", cogerCarta(jugador));
+		        	if(puntuacionJugador(jugador) > 21){
+		        		printf("Te has pasado\n\n");
+		        		break;//termina partida
+		        	}
+        		} else if (opcion == 'p'){
+		        	cogerCartasCrupier(crupier);
+		        	comprobarGanador(crupier, jugador);
+		        	break;
+        		} else if (opcion == 'd'){
+        			printf("Has doblado. Te sale la carta %d\n", cogerCarta(jugador));
+		        	cogerCartasCrupier(crupier);
+		        	comprobarGanador(crupier, jugador);
+		        	break;
+        		}
+        	}//terminar una partida
+
+        	printf("¿Quieres volver a jugar?\n");
+        	printf("Opciones: Si (s), No (n)\n");
+ 			scanf(" %c", &opcion);
+ 			if(opcion == 'n'){//irte de la mesa
+ 				partida = 0;
+ 			}
+
+        }
+        printf("Nos vemos\n");
+
+        return 0;
+}
